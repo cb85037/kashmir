c20c8c1b385bef6e7d0d802d9b53ac34765c5a8f
cb85037 <cristianbessone@gmail.com>
2017-11-10 09:22:41 -0300

    After apache config No more Errors in Project and Libraries

--- cliente_sucursal_web/src/servlets/CajeroServlet.java
+++ cliente_sucursal_web/src/servlets/CajeroServlet.java
@@ -4,6 +4,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 //import javax.servlet.RequestDispatcher;
 //import javax.servlet.ServletException;
 /*import javax.servlet.http.HttpServlet;
